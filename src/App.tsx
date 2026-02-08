2d29100850b4208de383bef952a6e8b51f8cda74
klglondon-26 <klg.london@gmail.com>
2026-02-08 16:24:51 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
 
-<Routes>
-  <Route path="/" element={<Index />} />
-  <Route path="/index.html" element={<Navigate to="/" replace />} />
-  <Route path="*" element={<NotFound />} />
-</Routes>
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/index.html" element={<Navigate to="/" replace />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
